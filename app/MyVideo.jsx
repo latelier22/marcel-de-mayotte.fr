9298e76d0061bb5415502f6237d62f349f2782ae
latelier22 <latelier.cyrille@gmail.com>
2024-04-04 21:53:11 +0200

    demarrage video à 10s

--- app/MyVideo.jsx
+++ app/MyVideo.jsx
@@ -1,23 +1,52 @@
-const myComponent = () => {
+"use client";
+
+// VideoPlayer.js
+import React, { useRef, useEffect, useState } from 'react';
+import styles from './VideoPlayer.module.css'; // Importez vos styles CSS
+
+const VideoPlayer = () => {
+  const [showText, setShowText] = useState(true);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.currentTime = 10.5; // Démarrer la lecture à partir de 1 minute 20 secondes (80 secondes)
+    }
+  }, []);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const handlePlay = () => {
+      setShowText(false); // Faites disparaître le texte lorsque la vidéo est en lecture
+    };
+
+    const handlePause = () => {
+      setShowText(true); // Affichez à nouveau le texte lorsque la vidéo est mise en pause
+    };
+
+    if (video) {
+      video.addEventListener('play', handlePlay);
+      video.addEventListener('pause', handlePause);
+    }
+
+    return () => {
+      if (video) {
+        video.removeEventListener('play', handlePlay);
+        video.removeEventListener('pause', handlePause);
+      }
+    };
+  }, []);
+
   return (
-    <>
-     <header>
- 
- <div>
- <div className="container mx-auto px-5 py-2 lg:pt-4 ">
-      <h1>Ma vidéo</h1>
-      <video controls>
-        <source
-          src="https://marcel-de-mayotte.latelier22.fr/videos/videomarcel-sd.mp4"
-          type="video/mp4"
-        />
-        Votre navigateur ne prend pas en charge la vidéo.
+    <div className={styles.container}>
+      <video ref={videoRef} controls>
+        <source src="https://marcel-de-mayotte.latelier22.fr/videos/videomarcel-sd.mp4" />
+        Votre navigateur ne prend pas en charge la lecture de vidéos.
       </video>
-      </div>
-      </div>
-      </header>
-    </>
+      {showText && <div className={styles.overlay}>Entretien de Marcel Séjour</div>}
+    </div>
   );
 };
 
-export default myComponent;
+export default VideoPlayer;
