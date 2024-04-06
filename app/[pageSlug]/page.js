8aff34046b1084f7c187ef20df12886fb1fdcad1
latelier22 <latelier.cyrille@gmail.com>
2024-04-06 15:27:06 +0200

    qui suis-je

--- app/[pageSlug]/page.js
+++ app/[pageSlug]/page.js
@@ -1,4 +1,3 @@
-// MyPage.js
 
 import React from "react";
 import Navbar from "../NavBar";
@@ -7,40 +6,38 @@ import RootLayout from "../layout";
 import HeaderSimple from "../headerSimple";
 import Section from "../Section";
 import Cards from "../Cards";
-import MyLightBox from "../MyLightBox"
+import MyLightBox from "../MyLightBox";
 import { Pages, site } from "../site";
+import { permanentRedirect } from 'next/navigation'
 
 const MyPage = ({ params }) => {
   const pageSlug = params.pageSlug;
   const page = Pages[pageSlug];
+
+  // Check if the pageSlug is 'boutique' and redirect to 'boutique.marcel-de-mayotte.fr'
+  if (pageSlug === "boutique") {
+    permanentRedirect("https://boutique.marcel-de-mayotte.fr");
+    return null; // Return null as we don't need to render anything in this case
+  }
+
   if (!page) {
     return (
-      <RootLayout >
+      <RootLayout>
         <div className="min-h-screen flex flex-col justify-center items-center">
           <h1>Page non trouvée</h1>
         </div>
       </RootLayout>
     );
   }
+  
   return (
     <RootLayout pageTitle={page.title} pageDescription={page.description}>
-
       <Navbar />
-
-      <HeaderSimple photos={page.photos} siteTitle = {site.title} title={page.title}/>
-
-      {/* <ImagesBar photos={page.photos} /> */}
+      <HeaderSimple photos={page.photos} siteTitle={site.title} title={page.title}/>
       <MyLightBox photos={page.photos} />
-
-      {/* {page.sections.map((section, index) => (
-        <Section key={index} section={section} />
-      ))} */}
-     
-
       <div className="">
-        <Cards cards={page.cards}  />
+        <Cards cards={page.cards} />
       </div>
-
       <Footer />
     </RootLayout>
   );
