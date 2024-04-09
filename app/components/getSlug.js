354f30a495ea827612a20d81548a64bdc337ee31
latelier22 <latelier.cyrille@gmail.com>
2024-04-09 09:23:02 +0200

    install slugify

--- app/components/getSlug.js
+++ app/components/getSlug.js
@@ -1,8 +1,15 @@
-export default function slugify(text) {
-    return text.toString().toLowerCase().trim()
-      .replace(/\s+/g, '-')         // Remplace les espaces par des tirets
-      .replace(/[^\w\-]+/g, '')     // Supprime tous les caractères non word, non dash
-      .replace(/\-\-+/g, '-');      // Remplace multiple dashes par un seul tiret
+import slugify from 'slugify';
+
+export default function getSlug(text) {
+
+  const originalString = text;
+  return slugify(originalString, { lower: true });
+
+
+  // return text.toString().toLowerCase().trim()
+    //   .replace(/\s+/g, '-')         // Remplace les espaces par des tirets
+    //   .replace(/[^\w\-]+/g, '')     // Supprime tous les caractères non word, non dash
+    //   .replace(/\-\-+/g, '-');      // Remplace multiple dashes par un seul tiret
   }
 
 
