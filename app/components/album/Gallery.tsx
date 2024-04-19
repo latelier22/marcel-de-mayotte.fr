f0c8ea467cc6e88a9071e6311b02b77de56b138a
latelier22 <latelier.cyrille@gmail.com>
2024-04-19 11:06:28 +0200

    lightbox ok pb zoom

--- app/components/album/Gallery.tsx
+++ app/components/album/Gallery.tsx
@@ -1,17 +1,37 @@
 "use client";
 
+import { useState } from "react";
+
 import PhotoAlbum from "react-photo-album";
 import NextJsImage from "./NextJsImage";
 
-const Gallery = ({photos}) => {
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+const Gallery = ({ photos }) => {
+  const [index, setIndex] = useState(-1);
   return (
-    <PhotoAlbum
-      layout="rows"
-      photos={photos}
-      renderPhoto={NextJsImage}
-      defaultContainerWidth={1200}
-      sizes={{ size: "calc(100vw - 240px)" }}
-    />
+    <>
+       <PhotoAlbum photos={photos} layout="rows" targetRowHeight={350} onClick={({ index }) => setIndex(index)} />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+    </>
+
   );
 }
 
