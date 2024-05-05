abfe164c02690871848c018a0a9c7959e97a718a
latelier22 <latelier.cyrille@gmail.com>
2024-05-05 05:39:05 +0200

    modif import Eye in togglevivibilityButton

--- app/components/album/icons/VisibilityToggleButton.jsx
+++ app/components/album/icons/VisibilityToggleButton.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleVisibility } from '../../../lib/features/auth/visibleSlice'; // Assurez-vous que le chemin d'importation est correct
-import Eye from './Eye';
+import {Eye} from "./index"
 
 function VisibilityToggleButton( {isAmin}) {
   const isVisible = useSelector((state) => state.visible.isVisible);
