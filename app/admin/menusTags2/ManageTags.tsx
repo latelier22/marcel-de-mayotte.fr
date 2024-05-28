df9e81ed4300e50835c959deb7313a001d355566
latelier22 <latelier.cyrille@gmail.com>
2024-05-28 13:56:49 +0200

    try to update order

--- app/admin/menusTags2/ManageTags.tsx
+++ app/admin/menusTags2/ManageTags.tsx
@@ -1,12 +1,10 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import { DndContext, closestCenter, useSensor, MouseSensor } from '@dnd-kit/core';
+import { DndContext, closestCenter } from '@dnd-kit/core';
 import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import useMenuStore from '../../store/useStore';
-import myFetch from '../../components/myFetch';
-
 import {
   SimpleTreeItemWrapper,
   SortableTree,
@@ -16,29 +14,25 @@ import {
 
 const ManageTags = () => {
   const menuItems = useMenuStore((state) => state.menuItems);
+  const fetchAndSetMenus = useMenuStore((state) => state.fetchAndSetMenus);
   const setMenus = useMenuStore((state) => state.setMenus);
+  const updateMenuItems = useMenuStore((state) => state.updateMenuItems);
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchAndSetMenus();
+  }, []);
 
   useEffect(() => {
     setItems(transformMenuItemsToTreeItems(menuItems));
   }, [menuItems]);
 
-  const handleItemsChanged = (updatedItems) => {
+  const handleItemsChanged = async (updatedItems) => {
     setItems(updatedItems);
     const updatedMenuItems = transformTreeItemsToMenuItems(updatedItems);
     setMenus(updatedMenuItems);
-    persistMenus(updatedMenuItems);
-    console.log('Updated menuItems in store:', updatedMenuItems); // Log the updated state
-  };
-
-  const persistMenus = async (menus) => {
-    const payload = { data: menus };
-    try {
-      await myFetch(`/api/menus/  `, 'PUT', payload);
-      console.log('Menus updated successfully.');
-    } catch (error) {
-      console.error('Failed to update menus:', error);
-    }
+    await updateMenuItems(updatedMenuItems);
+    console.log('Updated menuItems in store and database:', updatedMenuItems); // Log the updated state
   };
 
   return (
@@ -71,12 +65,13 @@ const TreeItem = React.forwardRef<
 /*
  * Fonction pour transformer les données de menu en une structure compatible avec TreeItems
  */
-const transformMenuItemsToTreeItems = (menuItems) => {
-  return menuItems.map((item) => ({
+const transformMenuItemsToTreeItems = (menuItems, parentId = '') => {
+  return menuItems.map((item, index) => ({
     id: item.id,
     value: item.label,
     route: item.route,
-    children: item.children ? transformMenuItemsToTreeItems(item.children) : [],
+    order: item.order,
+    children: item.children ? transformMenuItemsToTreeItems(item.children, item.id) : [],
   }));
 };
 
@@ -84,10 +79,11 @@ const transformMenuItemsToTreeItems = (menuItems) => {
  * Fonction pour transformer les TreeItems en données de menu
  */
 const transformTreeItemsToMenuItems = (treeItems) => {
-  return treeItems.map(item => ({
+  return treeItems.map((item, index) => ({
     id: item.id,
     label: item.value,
     route: item.route,
+    order: index, // Mise à jour de l'ordre ici
     children: item.children ? transformTreeItemsToMenuItems(item.children) : [],
   }));
 };
--- app/store/useStore.js
+++ app/store/useStore.js
@@ -7,14 +7,58 @@ const useMenuStore = create((set) => ({
     console.log('Setting menus:', menus); // Log to see the updated state
     set({ menuItems: menus });
   },
+  fetchAndSetMenus: async () => {
+    const endpoint = `/api/menus?populate=*`;
+    const response = await myFetch(endpoint, 'GET', null, 'menus');
+
+    let { data: menuItems } = response;
+
+    // Fonction récursive pour transformer les données
+    const transformMenuItem = (item) => {
+      return {
+        id: item.id,
+        label: item.attributes?.label,
+        route: item.attributes?.route,
+        order: item.attributes?.order,
+        children: item.attributes?.children?.data.map(transformMenuItem) || [],
+        parent: item.attributes?.parent?.data?.id || null
+      };
+    };
+
+    // Appliquer la transformation aux données du menu
+    menuItems = menuItems.map(transformMenuItem);
+
+    // Filtrer les éléments de menu pour ne garder que ceux sans parent
+    const rootMenuItems = menuItems.filter(item => !item.parent);
+
+    // Trier les éléments de menu par le champ "order"
+    rootMenuItems.sort((a, b) => a.order - b.order);
+
+    // Trier les sous-menus par le champ "order" également
+    const sortChildren = (items) => {
+      items.forEach(item => {
+        if (item.children) {
+          item.children.sort((a, b) => a.order - b.order);
+          sortChildren(item.children);
+        }
+      });
+    };
+
+    sortChildren(rootMenuItems);
+
+    set({ menuItems: rootMenuItems });
+  },
   updateMenuItems: async (menus) => {
     try {
-      const response = await myFetch('/api/menus', 'PUT', { menus }, 'menus');
-      if (response.ok) {
-        console.log('Menus updated successfully');
-      } else {
-        console.error('Failed to update menus', response.statusText);
+      for (const menu of menus) {
+        await myFetch(`/api/menus/${menu.id}`, 'PUT', { data: menu });
+        if (menu.children && menu.children.length > 0) {
+          for (const child of menu.children) {
+            await myFetch(`/api/menus/${child.id}`, 'PUT', { data: child });
+          }
+        }
       }
+      console.log('Menus updated successfully');
     } catch (error) {
       console.error('Error updating menus:', error);
     }
