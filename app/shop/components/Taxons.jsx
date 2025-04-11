87de2dd527c85bf8cba9c7f4af2f2c881927abbc
Cyrille <latelier.cyrille@gmail.com>
2025-04-11 10:27:59 +0200

    modif accueil : remis Taxons Ajout ds navbar menu BOUTIQUE si /shop ds route de menu alors shopUrl

--- app/shop/components/Taxons.jsx
+++ app/shop/components/Taxons.jsx
@@ -7,12 +7,12 @@ const baseUrl = process.env.NEXT_PUBLIC_SHOP_URL;
 
 export default function Taxons({ taxons }) {
   return (
-    <div className="container mx-auto my-12 p-8 rounded-md dark:bg-black border-gold-500 border-solid border-2 animate-fadeInOut">
+    <div className="container mx-auto my-12 p-8 rounded-md bg-cyan-500 border-gold-500 border-solid border-2 animate-fadeInOut">
       
       {/* Titre centré */}
       <Link href={`${baseUrl}`}>
-        <h2 className="text-2xl font-bold mb-8 text-center text-white">
-          Retrouvez mes œuvres en reproduction haute définition et en produits dérivés de qualité sur ma boutique en ligne
+        <h2 className="text-2xl font-bold mb-8 text-center text-orange-600 hover:text-yellow-900 transition">
+          Retrouvez mes œuvres en reproduction haute définition <br/> et en produits dérivés de qualité sur ma boutique en ligne
         </h2>
       </Link>
       
@@ -21,7 +21,7 @@ export default function Taxons({ taxons }) {
         {taxons.map((taxon) => (
           <li key={taxon.id}>
             <Link href={`${baseUrl}/fr_FR/taxons/${taxon.slug}`}>
-              <span className="inline-block px-6 py-3 border border-white rounded-md text-white hover:bg-white hover:text-black transition">
+              <span className="inline-block px-6 py-3 border border-orange-600 bg-yellow-500 rounded-md text-orange-600 hover:bg-orange-600 hover:text-yellow-500 transition">
                 {taxon.name}
               </span>
             </Link>
