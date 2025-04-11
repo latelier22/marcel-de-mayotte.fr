e5e28761e775f99c6b1b06a55c93e47682172d96
latelier22 <latelier.cyrille@gmail.com>
2025-04-11 18:34:44 +0200

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Script de déploiement pour marcel-de-mayotte.fr
 
 ssh debian@vps.latelier22.fr << 'EOF'
-  cd /marcel-de-mayotte.fr
+  cd ~/marcel-de-mayotte.fr
   git pull
   sudo pnpm run build
   pm2 restart 0
