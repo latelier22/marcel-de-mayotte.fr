a7230bfc82fe7972e12fc85ffe8733f78d8370c3
latelier22 <latelier.cyrille@gmail.com>
2025-11-16 10:52:31 +0000

    suppression taxons et prépa

--- app/accueil/page.js
+++ app/accueil/page.js
@@ -41,13 +41,13 @@ async function Accueil() {
 
       <Citation citations={citations} section={sections[0]} />
       
-      <Taxons taxons={taxons} /> 
+      {/* <Taxons taxons={taxons} />  */}
       {/* 👈 Ajout de la liste des taxons */}
       {/* <ProductCards products={productVariantsFront} /> */}
 
       <Book3d />
 
-      <ImageSlider images={prepaImages} title={"Préparatifs de l'exposition à Sallertaine, du 4 au 6 avril 2025"} slug={"sallertaine-preparation"}/> 
+      {/* <ImageSlider images={prepaImages} title={"Préparatifs de l'exposition à Sallertaine, du 4 au 6 avril 2025"} slug={"sallertaine-preparation"}/>  */}
 
 
       <Banner photo={photos[0]} />
